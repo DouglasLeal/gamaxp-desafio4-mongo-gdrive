2ae7979ae8377e72dd2ef41e200773b528c4d932
Douglas Leal <douglas.leal@live.com>
2023-02-25 12:24:31 -0300

    Crud categorias criado

--- src/rotas/index.js
+++ src/rotas/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 
 import authRotas from "./authRotas.js";
-
+import categoriaRotas from "./categoriaRotas.js";
 import usuarioRotas from "./usuarioRotas.js";
 
 const routes = (app) => {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use("/", authRotas);
+    app.use("/categorias", categoriaRotas);
     app.use("/usuarios", usuarioRotas);      
 }
 
