6bb5388452d116e886b0a396074174c87b01931b
Douglas Leal <douglas.leal@live.com>
2023-02-25 11:08:20 -0300

    CRUD usuario criado

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+import db from "./config/db.js";
+import routes from "./rotas/index.js";
+import HandleErrorMiddleware from "./middlewares/HandleErrorMiddleware.js";
+
+db.on("error", console.log.bind(console, "DB - Erro ao conectar."));
+db.once("open", () => {
+    console.log("DB - Conectado com sucesso.");
+});
+
+const app = express();
+
+routes(app);
+
+app.use(HandleErrorMiddleware.handle);
+
+export default app;
